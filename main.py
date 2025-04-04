8366adba7f565c1a11f611e226df91af982d8b50
bkandh30 <kandhari.bhavya20@gmail.com>
2025-04-04 15:16:27 -0700

    parse question section

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import socket
 import struct
 
-domain_name = b"\x0c" + b"codecrafters" + b"\x02" + b"io" + b"\x00"
-
-def build_dns_response(query_packet: bytes) -> bytes:
+def parse_header(query_packet: bytes):
     #Parse the DNS header
     query_id = query_packet[0:2]
     flags = struct.unpack(">H", query_packet[2:4])[0]
@@ -18,6 +16,43 @@ def build_dns_response(query_packet: bytes) -> bytes:
     rcode = 0 if opcode == 0 else 4
     response_flags |= rcode
 
+    return query_id, response_flags
+
+def parse_question(query_packet: bytes):
+    #Extract the Question section
+    #Skip 12 bytes
+    position = 12
+
+    domain_name = bytearray()
+
+    #Extract Domain Name
+    while True:
+        length = query_packet[position]
+        if length == 0:
+            domain_name.append(0)
+            position += 1
+            break
+    
+        domain_name.append(length)
+        position += 1
+        domain_name.extend(query_packet[position:position+length])
+        position += length
+    
+    #Extract type and class
+    type_bytes = query_packet[position:position+2]
+    position += 2
+    class_bytes = query_packet[position:position+2]
+    position += 2
+    
+    question = bytes(domain_name) + type_bytes + class_bytes
+
+    domain_name = bytes(domain_name)
+
+    return question, domain_name
+
+def build_dns_response(query_packet: bytes) -> bytes:
+    query_id, response_flags = parse_header(query_packet)
+    
     transaction_id = query_id
     header_flags = struct.pack(">H", response_flags)
     qdcount = struct.pack(">H", 1)            # Number of questions
@@ -27,10 +62,7 @@ def build_dns_response(query_packet: bytes) -> bytes:
 
     header = transaction_id + header_flags + qdcount + ancount + nscount + arcount
 
-    question = domain_name
-    question += struct.pack(">H", 1)  # Type: A
-    question += struct.pack(">H", 1)  # Class: IN
-
+    question, domain_name = parse_question(query_packet)
 
     adomain = domain_name
     atype = struct.pack(">H", 1)        # Type: A
