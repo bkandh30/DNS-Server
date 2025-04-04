a3dfa600fcbda6080033027fbfe8029b1330fb57
bkandh30 <kandhari.bhavya20@gmail.com>
2025-04-03 21:15:20 -0700

    create udp server and initialize the header section

---
+++ main.py
@@ -0,0 +1,26 @@
+import socket
+
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    # Uncomment this block to pass the first stage
+    
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("127.0.0.1", 2053))
+    
+    while True:
+        try:
+            buf, source = udp_socket.recvfrom(512)
+    
+            response = b"\x04\xd2\x80" + (b"\x00" * 9)
+    
+            udp_socket.sendto(response, source)
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            break
+
+
+if __name__ == "__main__":
+    main()
